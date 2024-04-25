ffdf4db734a99bb94378f4b4603e9e5b1e0e9e89
beingzaidmasood <144796346+beingzaidmasood@users.noreply.github.com>
2024-04-26 00:56:00 +0530

    Add files via upload

---
+++ untoldcoding.js
@@ -0,0 +1,35 @@
+const text =
+  "Dear Pathan Insha Khan ,  I wnna Say Something.         you are a smart girl, but you never give me appointments on time!!!! kindly give me an appointment at the earliest possible time....   Looking forward to see you!!!! Byeeee Thankyou";
+
+const paragraph = text.split("");
+
+let i = 0;
+
+function dashOut(arr) {
+  if (i < arr.length) {
+    console.log(arr[i]);
+    document.querySelector(".textCont").textContent += arr[i];
+
+    i++;
+    console.log("The i count: " + i);
+    setTimeout(function () {
+      dashOut(arr);
+    }, interval(arr[i]));
+  }
+}
+
+function interval(letter) {
+  console.log(letter);
+  if (letter == ";" || letter == "." || letter == ",") {
+    return Math.floor(Math.random() * 500 + 500);
+  } else {
+    return Math.floor(Math.random() * 130 + 5);
+  }
+}
+
+function startFromBegin() {
+  i = 0;
+  dashOut(paragraph);
+}
+
+startFromBegin();
